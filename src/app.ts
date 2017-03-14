79f979248b6be1484721d8f91fc5cf801eba1563
CHOU YUEHONG <joeyuehong@gmail.com>
2017-03-14 16:42:37 +0800

    Create

---
+++ src/app.ts
@@ -0,0 +1,46 @@
+import * as path from 'path';
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+
+class App{
+    public express:express.Application;
+
+    constructor(){
+        this.express = express();
+        this.middleware();
+        this.routes();
+    }
+
+    /**
+     * 
+     * 
+     * @private
+     * 
+     * @memberOf App
+     */
+    private middleware():void{
+        this.express.use(logger('dev'));
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({extended:false}));
+    }
+
+    /**
+     * 
+     * 
+     * @private
+     * 
+     * @memberOf App
+     */
+    private routes():void{
+        let router = express.Router();
+        router.get('/',(req,res,next)=>{
+            res.json({
+                message:'Hello World'
+            });
+        });
+        this.express.use('/',router);
+    }
+}
+
+export default new App().express;
